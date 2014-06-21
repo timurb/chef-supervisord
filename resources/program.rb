64709d42c4aa11125aa0093d792ed95dba242a6f
Timur Batyrshin <erthad@gmail.com>
2014-06-21 16:31:51 +0400

    added autorestart parameter

--- resources/program.rb
+++ resources/program.rb
@@ -4,6 +4,7 @@ attribute :name, :kind_of => String, :name_attribute => true
 attribute :command, :kind_of => String, :required => true
 
 attribute :autostart, :kind_of => [FalseClass, TrueClass], :default => false
+attribute :autorestart, :kind_of => [FalseClass, TrueClass], :default => false
 attribute :startsecs, :kind_of => Integer, :default => nil
 attribute :user, :kind_of => String, :default => nil
 attribute :directory, :kind_of => String, :default => nil
